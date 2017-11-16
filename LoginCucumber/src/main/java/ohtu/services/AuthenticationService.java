e03aa4366518140326ec40fcca79cc7d81b7bff8
Ville Hänninen <ville.hanninen@helsinki.fi>
2017-11-16 13:58:53 +0200

    tehtävä 8

--- LoginCucumber/src/main/java/ohtu/services/AuthenticationService.java
+++ LoginCucumber/src/main/java/ohtu/services/AuthenticationService.java
@@ -4,11 +4,15 @@ import ohtu.domain.User;
 import java.util.ArrayList;
 import java.util.List;
 import ohtu.data_access.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AuthenticationService {
 
     private UserDao userDao;
 
+    @Autowired
     public AuthenticationService(UserDao userDao) {
         this.userDao = userDao;
     }
@@ -46,14 +50,14 @@ public class AuthenticationService {
             return true;
         }
 
-        if  (password.length() < 8) {
+        if (password.length() < 8) {
             return true;
         }
-        
+
         if (password.matches("[a-zA-Z]+")) {
             return true;
         }
-        
+
         return false;
     }
 }
