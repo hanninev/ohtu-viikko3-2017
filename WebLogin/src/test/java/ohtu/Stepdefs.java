75ab8a7b7e445349de6798cec1709aab1c1e59e4
Ville Hänninen <ville.hanninen@helsinki.fi>
2017-11-17 13:30:25 +0200

    tehtävä 12

--- WebLogin/src/test/java/ohtu/Stepdefs.java
+++ WebLogin/src/test/java/ohtu/Stepdefs.java
@@ -4,6 +4,7 @@ import cucumber.api.java.After;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import java.util.Random;
 import static org.junit.Assert.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -48,6 +49,48 @@ public class Stepdefs {
         pageHasContent("Give your credentials to login");
     }
 
+    @Given("^command new user is selected$")
+    public void command_new_user_is_selected() throws Throwable {
+        driver.get(baseUrl);
+        WebElement element = driver.findElement(By.linkText("register new user"));
+        element.click();
+    }
+
+    @When("^a valid username \"([^\"]*)\" and password \"([^\"]*)\" and matching password confirmation are entered$")
+    public void a_valid_username_and_password_and_matching_password_confirmation_are_entered(String username, String password) throws Throwable {
+        signUpWith(username, password, password);
+    }
+
+    @Then("^a new user is created$")
+    public void a_new_user_is_created() throws Throwable {
+        pageHasContent("Welcome to Ohtu Application!");
+    }
+
+    @Then("^user is not created and error \"([^\"]*)\" is reported$")
+    public void user_is_not_created_and_error_is_reported(String errorMessage) throws Throwable {
+        pageHasContent(errorMessage);
+    }
+    
+        @When("^too short username \"([^\"]*)\" and valid password \"([^\"]*)\" are given$")
+    public void too_short_username_and_valid_password_are_given(String username, String password) throws Throwable {
+            signUpWith(username, password, password);
+    }
+
+    @When("^a valid username \"([^\"]*)\" and too short password \"([^\"]*)\" are given$")
+    public void a_valid_username_and_too_short_password_are_given(String username, String password) throws Throwable {
+        signUpWith(username, password, password);
+    }
+
+    @When("^already taken username \"([^\"]*)\" and valid password \"([^\"]*)\" are given$")
+    public void already_taken_username_and_valid_password_are_given(String username, String password) throws Throwable {
+        signUpWith(username, password, password);
+    }
+    
+    @When("^a valid username \"([^\"]*)\" is given, but a valid password \"([^\"]*)\" do not match with password confirmation \"([^\"]*)\"$")
+    public void a_valid_username_is_given_but_a_valid_password_do_not_match_with_password_confirmation(String username, String password, String confirmation) throws Throwable {
+        signUpWith(username, password, confirmation);
+    }    
+
     @After
     public void tearDown() {
         driver.quit();
@@ -65,6 +108,17 @@ public class Stepdefs {
         element = driver.findElement(By.name("password"));
         element.sendKeys(password);
         element = driver.findElement(By.name("login"));
+        element.submit();
+    }
+
+    private void signUpWith(String username, String password, String passwordConf) {
+        WebElement element = driver.findElement(By.name("username"));
+        element.sendKeys(username);
+        element = driver.findElement(By.name("password"));
+        element.sendKeys(password);
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys(passwordConf);
+        element = driver.findElement(By.name("signup"));
         element.submit();
     }
 }
